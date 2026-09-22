231045675d0c97afdf1c31407238c0dc16a35c4f
Hasna Nadia <www.hasnanadia02@gmail.com>
2026-09-22 21:15:33 +0600

    Update Supabase session pooler connection

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:ExpenseTrack2026Pass@db.bnxqelucepymhlxbhtoo.supabase.co:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.bnxqelucepymhlxbhtoo:ExpenseTrack2026Pass@aws-0-ap-northeast-2.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
